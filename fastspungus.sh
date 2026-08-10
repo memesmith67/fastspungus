72ffcc95fe5be4c3b1dfd55a186965b61767dee4
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-08-09 23:04:54 -0700

    Update fastspungus.sh

--- fastspungus.sh
+++ fastspungus.sh
@@ -2,11 +2,11 @@
 fastspungus(){ awk 'BEGIN{getline;
 print "void* "$1"(void* i){;int s=1;";
 print "while(s){switch(s){default:"}
-{if($1=="declare"){p=$2" "$3";"}
-else if($1=="assign"){p=$2"="$3$4$5";"}
-else if($1=="free"){p=$1"("$2");"}
-else if($1=="case"){p=$1" "$2":"}
-else if($1=="break"){p=$1";"}
-else if($2$3$5!~/^[a-zA-Z0-9_()*]*$/||
+{if($2$3$5!~/^[a-zA-Z0-9_()*]*$/||
 $4!~/^[<>\/*%&|!=|+-]*$/){exit 1}
-print p}END{p="}}}"}';};
+if($1=="declare"){print $2" "$3";"}
+else if($1=="assign"){print $2"="$3$4$5";"}
+else if($1=="free"){print $1"("$2");"}
+else if($1=="case"){print $1" "$2":"}
+else if($1=="break"){print $1";"}
+else{exit 1}}END{p="}}}"}';};
