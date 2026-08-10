eb4ac71cb1cd2b38e2f465206a0a70f1f5e9168e
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-08-10 01:24:05 -0700

    Update fastspungus.sh

--- fastspungus.sh
+++ fastspungus.sh
@@ -1,12 +1,18 @@
 #copyright john morris beck 2026 gpl2
-fastspungus(){ awk 'BEGIN{getline;
-print "void* "$1"(void* i){;int s=1;";
-print "while(s){switch(s){default:"}
-{if($2$3$5!~/^[a-zA-Z0-9_()*]*$/||
-$4!~/^[<>\/*%&|!=|+-]*$/){exit 1}
-if($1=="declare"){print $2" "$3";"}
-else if($1=="assign"){print $2"="$3$4$5";"}
-else if($1=="free"){print $1"("$2");"}
-else if($1=="case"){print $1" "$2":"}
-else if($1=="break"){print $1";"}
-else{exit 1}}END{p="}}}"}';};
+fastspungus(){ awk '
+function f(x,y){if($1==x){print y;q=0}}
+$0=="fastspungus"{s=!s;next}!s;s{
+q=1;a=$1;b=$2;c=$3;
+f("function","void* "b"(void* "c"){")
+f("declare",b" "c";")
+f("assign",b"="c$4$5";")
+f("free",a"("b");")
+f("case",a" "b":")
+f("break",a";")
+f("while",a"("b"){")
+f("struct",a"("b"){")
+f("}",a)
+f("default",a":")
+if(q||b c$5!~/^[a-zA-Z0-9_()*]*$/||
+$4!~/^[<>\/*%&|!=|+-]*$/){
+print ";=;";exit 1}}';};
