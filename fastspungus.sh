d0c50db17fc61bbb464426622876611c7858aed6
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-08-11 01:15:47 -0700

    Update fastspungus.sh

--- fastspungus.sh
+++ fastspungus.sh
@@ -6,13 +6,13 @@ q=1;a=$1;b=$2;c=$3;
 f("function","void* "b"(void* "c"){")
 f("declare",b" "c";")
 f("assign",b"="c$4$5";")
-f("free",a"("b");")
+f("free(",a b");")
 f("case",a" "b":")
-f("break",a";")
-f("while",a"("b"){")
-f("struct",a"("b"){")
+f("break;",a")
+f("while(",a b"){")
+f("switch",a b"){")
 f("}",a)
-f("default",a":")
+f("default:",a)
 if(q||b c$5!~/^[a-zA-Z0-9_()*]*$/||
 $4!~/^[<>\/*%&|!=|+-]*$/){
 print ";=;";exit 1}}';};
