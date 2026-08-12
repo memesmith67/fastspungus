278878459e8156df184515c77c9449e2516acbf7
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-08-12 14:42:09 -0700

    Update fastspungus.sh

--- fastspungus.sh
+++ fastspungus.sh
@@ -1,12 +1,13 @@
 #copyright john morris beck 2026 gpl2
 fastspungus(){ awk '
-function n(x){return x~/^[a-zA-Z0-9_*]+$/?x:";=;"}
+function n(x){return x~/^[a-zA-Z0-9_'\'',]+$/?x:";=;"}
+function t(x){return x~/^[a-zA-Z0-9_*]+$/?x:";=;"}
 function o(x){return x~/^[<>\/*%&|!=|+\-]+$/?x:";=;"}
 $0=="fastspungus"{s=!s;next}!s;s{
 q=1;a=$1;b=n($2);c=n($3);
-if("function"==$1){print b" "c"("n($4)" "n($5)"){"}
-else if("declare"==$1){print b" "c"=0;"}
-else if("cast"==$1){print b"=("c")"n($4)";"}
+if("function"==$1){print t($2)" "c"("t($4)" "n($5)"){"}
+else if("declare"==$1){print t($2)" "c"=0;"}
+else if("cast"==$1){print b"=("t($3)")"n($4)";"}
 else if("assign"==$1){print b"="c o($4)n($5)";"}
 else if("get"==$1){print b"=*"c";"}
 else if("set"==$1){print "*"b"="c";"}
