41afcf91afcb714089b5a8c4cff63e671957d672
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-08-13 04:27:03 -0700

    Update fastspungus.sh

--- fastspungus.sh
+++ fastspungus.sh
@@ -1,10 +1,9 @@
 #copyright john morris beck 2026 gpl2
 fastspungus(){ awk '
-function n(x){return x~/^[a-zA-Z0-9_,]+$/?x:";=;"}
-function t(x){return x~/^[a-zA-Z0-9_*]+$/?x:";=;"}
-function o(x){return x~/^[<>\/*%&|!=|+\-]+$/?x:";=;"}
-$0=="fastspungus"{s=!s;next}!s;s{
-q=1;a=$1;b=n($2);c=n($3);
+function n(x){return x~/^[a-zA-Z0-9_,]+$/?x:";="}
+function t(x){return x~/^[a-zA-Z0-9_*]+$/?x:";="}
+function o(x){return x~/^[<>\/*%&|!=|+\-]+$/?x:";="}
+$0=="fastspungus"{s=!s;next}!s;s{q=1;a=$1;b=n($2);c=n($3);
 if("function"==$1){print t($2)" "c"("t($4)" "n($5)"){"}
 else if("declare"==$1){print t($2)" "c"=0;"}
 else if("cast"==$1){print b"=("t($3)")"n($4)";"}
@@ -19,5 +18,6 @@ else if("while"==$1){print a"("b"){"}
 else if("switch"==$1){print a"("b"){"}
 else if("}"==$1){print a}
 else if("default:"==$1){print a}
-else{print ";=;"}
-}';};
+else if("subleq"==$1){
+print "void "$1"(int *m){int t=*m;*m=(m[m[t+1]]-=m[m[t]])<1?m[t+2]:t+3;}"
+}else{print ";="}}';};
