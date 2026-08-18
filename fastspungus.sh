f6c22a6e0a099aa57ba20a151a2e509565a4ff84
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-08-18 04:12:06 -0700

    Update fastspungus.sh
    forgot ;

--- fastspungus.sh
+++ fastspungus.sh
@@ -1,11 +1,11 @@
 #copyright john morris beck 2026 gpl2
 fastspungus(){ awk '
-function n(x){return x~/^[a-zA-Z0-9_]+$/?x:e}
 function t(x){return x~/^[a-zA-Z0-9_*]+$/?x:e}
+function n(x){return x!~/\*/?x:e}
 function o(x){return x~/^[+\-<>\/*%&|!=]+$/?x:e}
 $0=="fastspungus"{s=!s;next}!s;s{e=";=";a=$1;b=n($2);c=n($3);
 if("function"==$1){print t($2)" "c"("t($4)" "n($5)"){"}
-else if("declare"==$1){print t($2)" "c"=0"}
+else if("declare"==$1){print t($2)" "c"=0;"}
 else if("cast"==$1){print b"=("t($3)")"n($4)";"}
 else if("assign"==$1){print b"="c o($4)n($5)";"}
 else if("get"==$1){print b"=*"c";"}
