99ac5cc3fe10a67ed05d194b3160b4a4ab4c31b9
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-08-18 04:20:01 -0700

    Update fastspungus.sh

--- fastspungus.sh
+++ fastspungus.sh
@@ -1,7 +1,7 @@
 #copyright john morris beck 2026 gpl2
 fastspungus(){ awk '
 function t(x){return x~/^[a-zA-Z0-9_*]+$/?x:e}
-function n(x){return x!~/\*/?x:e}
+function n(x){return x~/^[a-zA-Z0-9_]+$/?x:e}
 function o(x){return x~/^[+\-<>\/*%&|!=]+$/?x:e}
 $0=="fastspungus"{s=!s;next}!s;s{e=";=";a=$1;b=n($2);c=n($3);
 if("function"==$1){print t($2)" "c"("t($4)" "n($5)"){"}
